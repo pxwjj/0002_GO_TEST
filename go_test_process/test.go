0b004d0a29a0e0dfa464de98c9f641943d9bfbeb
pxwjj <wangpx@koal.com>
2023-06-09 10:24:51 +0800

    "string and printf"

--- go_test_process/test.go
+++ go_test_process/test.go
@@ -1,5 +1,8 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "runtime"
+)
 
 var (   //定义变量
     v1 int 
@@ -33,9 +36,21 @@ func GetName()(firstName,lastName,nickName string){
 func main(){
     //多重赋值
     //v1,v2 = v3,v4
-    
+    var (
+        str string
+    )
+    str = "hello world"
+    chone := str[0]
+
+    // str[0] = 'X'//错误，字符串赋值之后，不能再修改
+
+    fmt.Printf("The lenth of \"%s\" is %d \n" , str , len(str))   //Printf  Print  Println
+    fmt.Printf("The first character of \"%s\" is \"%c\" \n" , str , chone)
+
     _,_,nickName := GetName()
     fmt.Println(nickName)  //Chibi Maruko
     fmt.Println(a,b)  //1 2
     fmt.Println(Sunday , Monday , numberOfDays)  //0 1 7
+
+    fmt.Printf("My go version is \"%s\" \n" , runtime.Version())
 }
